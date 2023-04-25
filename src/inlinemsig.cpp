a1917cd66b9153599a243ca7de9b5efbcb8c79ab
Guillaume Babin-Tremblay <gbabint@msn.com>
2023-04-25 22:47:51 +0000

    bit of clean up

--- src/inlinemsig.cpp
+++ src/inlinemsig.cpp
@@ -3,23 +3,13 @@
 
 ACTION inlinemsig::test() {
 
-   transaction tx;
-
-   action a;
-
-   std::vector<permission_level> pla;
    permission_level pl;
 
    pl.actor = "inlinemsig11"_n;
    pl.permission = "active"_n;
 
+   std::vector<permission_level> pla;
    pla.push_back(pl);
-
-   a.account = "eosio.token"_n;
-
-   a.authorization = pla;
-
-   a.name = "transfer"_n;
 
    transfer_struct ts;
 
@@ -30,8 +20,14 @@ ACTION inlinemsig::test() {
 
    std::vector<char> b = pack(ts);
 
+   action a;
+
+   a.account = "eosio.token"_n;
+   a.authorization = pla;
+   a.name = "transfer"_n;
    a.data = b;
 
+   transaction tx;
    tx.actions.push_back(a);
 
    action act_propose(
